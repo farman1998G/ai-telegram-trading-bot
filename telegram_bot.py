c882609302f2a3b9051756f5863a41c71a55fe9d
farman1998G <farman.qenberov.98@mail.ru>
2026-07-18 03:03:56 +0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,16 @@
-from telegram import Bot
 import os
+import asyncio
+from telegram import Bot
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-bot = Bot(token=TOKEN)
-
-def start_bot():
-    bot.send_message(
+async def start_bot():
+    bot = Bot(token=TOKEN)
+    await bot.send_message(
         chat_id=CHAT_ID,
         text="🤖 AI Trading Bot uğurla başladı!"
     )
+
+if __name__ == "__main__":
+    asyncio.run(start_bot())
