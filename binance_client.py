8f929d2224eb564ef95957e22e0ba021d03e59f8
farman1998G <farman.qenberov.98@mail.ru>
2026-07-18 02:23:02 +0400

    Create binance_client.py

---
+++ binance_client.py
@@ -0,0 +1,11 @@
+from binance.client import Client
+import os
+
+API_KEY = os.getenv("BINANCE_API_KEY")
+API_SECRET = os.getenv("BINANCE_API_SECRET")
+
+client = Client(API_KEY, API_SECRET)
+
+def get_price(symbol="BTCUSDT"):
+    ticker = client.get_symbol_ticker(symbol=symbol)
+    return float(ticker["price"])
