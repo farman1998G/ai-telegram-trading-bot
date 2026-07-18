e374def6168de5af47b011bd40cf6580e40ed071
farman1998G <farman.qenberov.98@mail.ru>
2026-07-18 04:22:52 +0400

    Update binance_client.py

--- binance_client.py
+++ binance_client.py
@@ -1,10 +1,10 @@
 import requests
 
 def get_price(symbol="BTCUSDT"):
-    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 
     response = requests.get(url, timeout=10)
     response.raise_for_status()
 
     data = response.json()
-    return float(data["price"])
+    return float(data["bitcoin"]["usd"])
